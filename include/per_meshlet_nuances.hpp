5c0f593b375071ec0e1782e6298a21e01dc0cb41
Lukas Herzberger <herzberger.lukas@gmail.com>
2024-08-21 22:14:45 +0200

    add assimp, start building dag

---
+++ include/per_meshlet_nuances.hpp
@@ -0,0 +1,99 @@
+//
+// Created by lukas on 21.08.24.
+//
+
+#ifndef METIS_PER_MESHLET_NUANCES_HPP
+#define METIS_PER_MESHLET_NUANCES_HPP
+
+#include <iostream> // todo: remove
+#include <vector>
+
+#include "meshoptimizer.h"
+#include "metis.h"
+
+namespace pmn {
+    // todo: figure out API, move impl to private files
+    void generateNextLod() {
+
+    }
+
+    void create_dag(const std::vector<uint32_t> &indices, const std::vector<float> &vertices, size_t vertex_stride) {
+        // todo: should be params
+        const size_t max_vertices = 64;
+        const size_t max_triangles = 128;
+        const float cone_weight = 0.5f;
+
+        // todo: optimize mesh before generating meshlets
+
+        size_t max_meshlets = meshopt_buildMeshletsBound(indices.size(), max_vertices, max_triangles);
+        std::vector<meshopt_Meshlet> meshlets(max_meshlets);
+        std::vector<unsigned int> meshlet_vertices(max_meshlets * max_vertices);
+        std::vector<unsigned char> meshlet_triangles(max_meshlets * max_triangles * 3);
+
+        meshlets.resize(meshopt_buildMeshlets(
+                meshlets.data(),
+                meshlet_vertices.data(),
+                meshlet_triangles.data(),
+                indices.data(),
+                indices.size(),
+                vertices.data(),
+                vertices.size(),
+                vertex_stride,
+                max_vertices,
+                max_triangles,
+                cone_weight));
+
+        const meshopt_Meshlet& last = meshlets.back();
+        meshlet_vertices.resize(last.vertex_offset + last.vertex_count);
+        meshlet_triangles.resize(last.triangle_offset + ((last.triangle_count * 3 + 3) & ~3));
+
+        for (const auto& meshlet : meshlets) {
+            meshopt_optimizeMeshlet(&meshlet_vertices[meshlet.vertex_offset],
+                                    &meshlet_triangles[meshlet.triangle_offset],
+                                    meshlet.triangle_count,
+                                    meshlet.vertex_count);
+        }
+
+        double avg_vertices = 0;
+        double avg_triangles = 0;
+        size_t not_full = 0;
+
+        for (size_t i = 0; i < meshlets.size(); ++i)
+        {
+            const meshopt_Meshlet& m = meshlets[i];
+
+            avg_vertices += m.vertex_count;
+            avg_triangles += m.triangle_count;
+            not_full += m.vertex_count < max_vertices;
+        }
+
+        avg_vertices /= double(meshlets.size());
+        avg_triangles /= double(meshlets.size());
+
+        printf("Meshlets: %d meshlets (avg vertices %.1f, avg triangles %.1f, not full %d)\n",
+               int(meshlets.size()), avg_vertices, avg_triangles, int(not_full));
+
+        // todo: clean meshlets: move triangles with one or less shared vertices to other meshlet
+
+        std::vector<meshopt_Bounds> bounds{};
+        bounds.reserve(meshlets.size());
+        for (const auto& meshlet : meshlets) {
+            bounds.push_back(meshopt_computeMeshletBounds(&meshlet_vertices[meshlet.vertex_offset],
+                                                          &meshlet_triangles[meshlet.triangle_offset],
+                                                          meshlet.triangle_count,
+                                                          vertices.data(),
+                                                          vertices.size(),
+                                                          vertex_stride));
+        }
+
+        // todo:
+        // compute boundary for each cluster
+        // group up to 4 clusters based on number of shared boundary edges
+        // merge clusters in group (deduplicate)
+        // simplify to 50% of tris in group
+        // split groups into 2 clusters of n triangles each
+        // repeat
+    }
+}
+
+#endif //METIS_PER_MESHLET_NUANCES_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <string>
 
-#include "meshoptimizer.h"
-#include "metis.h"
+#include "assimp/Importer.hpp"
+#include "assimp/postprocess.h"
+#include "assimp/scene.h"
+
+#include "per_meshlet_nuances.hpp"
 
 int main() {
-    std::cout << "Hello, World! " << meshopt_buildMeshletsBound(2001, 255, 512) << " " << METIS_DBG_COARSEN << std::endl;
+    const std::string meshPath = "/home/lukas/Projects/per-meshlet-nuances/assets/stanford_bunny.obj";
+
+    Assimp::Importer importer;
+    const struct aiScene* scene = importer.ReadFile(meshPath,
+                      aiProcess_CalcTangentSpace |
+                      aiProcess_Triangulate |
+                      aiProcess_JoinIdenticalVertices |
+                      aiProcess_SortByPType);
+
+    const size_t vertex_stride = (3 * 4) * 4;
+    std::vector<float> vertices{};
+    std::vector<uint32_t> indices{};
+    for (int i = 0; i < scene->mMeshes[0]->mNumVertices; ++i) {
+        vertices.push_back(scene->mMeshes[0]->mVertices[i].x);
+        vertices.push_back(scene->mMeshes[0]->mVertices[i].y);
+        vertices.push_back(scene->mMeshes[0]->mVertices[i].z);
+        vertices.push_back(scene->mMeshes[0]->mNormals[i].x);
+        vertices.push_back(scene->mMeshes[0]->mNormals[i].y);
+        vertices.push_back(scene->mMeshes[0]->mNormals[i].z);
+        vertices.push_back(scene->mMeshes[0]->mTangents[i].x);
+        vertices.push_back(scene->mMeshes[0]->mTangents[i].y);
+        vertices.push_back(scene->mMeshes[0]->mTangents[i].z);
+        vertices.push_back(scene->mMeshes[0]->mBitangents[i].x);
+        vertices.push_back(scene->mMeshes[0]->mBitangents[i].y);
+        vertices.push_back(scene->mMeshes[0]->mBitangents[i].z);
+    }
+    for (int i = 0; i < scene->mMeshes[0]->mNumFaces; ++i) {
+        for (int j = 0; j < scene->mMeshes[0]->mFaces[i].mNumIndices; ++j) {
+            indices.push_back(scene->mMeshes[0]->mFaces[i].mIndices[j]);
+        }
+    }
+
+    pmn::create_dag(indices, vertices, vertex_stride);
+
+    std::cout << vertices.size() << " " << indices.size() << std::endl;
+
     return 0;
 }
